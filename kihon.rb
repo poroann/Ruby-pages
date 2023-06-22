95787a88c5e0fec32940916478b2314809203e06
tori <136542147+poroann@users.noreply.github.com>
2023-06-22 09:38:41 +0900

    Update kihon.rb

--- kihon.rb
+++ kihon.rb
@@ -1,1 +1,35 @@
+#!/usr/local/bin/ruby2.6
+
+    
+whtml=DATA.read
+
+
+print "Content-type:text/html\n\n"
+print whtml
+    
+    
+__END__
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<title>test</title>
+<style>
+   body{
+      background-color:white;}
+   div{
+      font-size:100px;}
+</style>
+</head>
+<body>
+<div>
+基本形
+</div>
+<script>
+</script>
+</body>
+</html>
 
