44543af214218703a1666ab9fd5a8d3ab01ad6c9
tori <136542147+poroann@users.noreply.github.com>
2023-06-22 18:12:08 +0900

    Add files via upload

---
+++ twi_tes1.rb
@@ -0,0 +1,40 @@
+#!/usr/local/bin/ruby
+print "Content-type:text/html\n\n"
+
+
+html=DATA.read
+pg_title=File.basename(__FILE__)
+
+
+html.gsub!("%title%",pg_title)
+html.gsub!("%本文%","本文")
+
+
+print html
+
+
+
+__END__
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<title>%title%</title>
+<style>
+   body{
+      background-color:white;}
+   div{
+      font-size:100px;}
+</style>
+</head>
+<body>
+<div>
+%本文%
+</div>
+<script>
+</script>
+</body>
+</html>
