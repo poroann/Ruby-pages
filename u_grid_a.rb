b0a419959a8eb2390b28e7bbe4d28a0e0e0ade94
tori <136542147+poroann@users.noreply.github.com>
2023-06-24 21:32:12 +0900

    Add files via upload

---
+++ u_grid_a.rb
@@ -0,0 +1,99 @@
+#!/usr/local/bin/ruby
+print "Content-type:text/html\n\n"
+
+
+html=DATA.read
+pg_mei=File.basename(__FILE__,".rb")
+
+
+css=<<EOS
+   body{
+      background-color:white;}
+   article{
+      height:100%;
+      font-size:100px;}
+   footer{
+      display:flex;
+      justify-content:center;
+      padding:5px;
+      background-color:gray;}
+   #qr{
+      width:100px;}
+   #tw{
+      width:30px;}
+   .group{
+      display:grid;
+      grid-template-columns:repeat(8,1fr);
+      grid-template-rows:repeat(8,1fr);
+      background-color:#eee;
+      padding:10px;}
+   .item{
+      padding:10px;
+      margin:10px;
+      color:#fff;
+      background-color:#2196f3;
+      border-radius:5px;}
+EOS
+
+
+header=<<EOS
+<h1>gridで盤面</h1>
+<p>test</p>
+<hr>
+EOS
+
+nav=<<EOS
+<p></p>
+EOS
+
+footer=<<EOS
+<img src='#{pg_mei}.png' id='qr'>
+<p>twitter link</p>
+<a href='https://twitter.com/poroann'><img src='./img/Twitter social icons - circle - blue.png' id='tw'></a>
+EOS
+
+
+w=""
+64.times do |i|
+   w+="<div class='item'>#{i}</div>\n"
+end
+
+text=<<EOS
+<div class='group'>
+#{w}
+</div>
+EOS
+
+
+html.gsub!("%css%","\n"+css)
+html.gsub!("%title%",pg_mei)
+html.gsub!("%header%","\n"+header)
+html.gsub!("%nav%","\n"+nav)
+html.gsub!("%本文%","\n"+text)
+html.gsub!("%footer%","\n"+footer)
+
+
+print html
+
+
+
+__END__
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<link rel="icon" href="/ico.png">
+<title>%title%</title>
+<style>%css%</style>
+</head>
+<body>
+<header>%header%</header>
+<nav>%nav%</nav>
+<main>%本文%</main>
+<footer>%footer%</footer>
+<script>
+</script>
+</body>
+</html>
